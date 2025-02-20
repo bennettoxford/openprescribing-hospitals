18cb1596ee96e823af09cb8e5701670703bba64f
Louis Fisher <louis.fisher1919@gmail.com>
2025-02-20 11:08:38 +0000

    reconfigure highcharts bundling

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte({
@@ -28,8 +26,38 @@ export default defineConfig({
         'collapsible-section': "./src/components/measures/CollapsibleSection.svelte",
         'chart': "./src/components/common/Chart.svelte",
         'product-details': "./src/components/dq/ProductDetails.svelte",
-      }
+      },
+      output: {
+        manualChunks: {
+          'highcharts': ['highcharts'],
+          'highcharts-modules': [
+            'highcharts/modules/stock',
+            'highcharts/highcharts-more',
+            'highcharts/modules/accessibility',
+            'highcharts/modules/exporting',
+            'highcharts/modules/export-data'
+          ]
+        }
+      },
     },
+  },
+  optimizeDeps: {
+    include: [
+      'highcharts',
+      'highcharts/highcharts-more',
+      'highcharts/modules/accessibility',
+      'highcharts/modules/exporting',
+      'highcharts/modules/export-data'
+    ]
+  },
+  resolve: {
+    alias: {
+      'highcharts': path.resolve(__dirname, 'node_modules/highcharts'),
+      'highcharts/highcharts-more': path.resolve(__dirname, 'node_modules/highcharts/highcharts-more.js'),
+      'highcharts/modules/accessibility': path.resolve(__dirname, 'node_modules/highcharts/modules/accessibility.js'),
+      'highcharts/modules/exporting': path.resolve(__dirname, 'node_modules/highcharts/modules/exporting.js'),
+      'highcharts/modules/export-data': path.resolve(__dirname, 'node_modules/highcharts/modules/export-data.js'),
+    }
   },
   server: {
     origin: "http://localhost:5173",
