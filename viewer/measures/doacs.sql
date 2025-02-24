3c28fd2f3e5c31e3af679efdb0be16d7a837d955
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-02-24 16:31:42 +0000

    Update doacs.sql

--- viewer/measures/doacs.sql
+++ viewer/measures/doacs.sql
@@ -9,30 +9,32 @@ WITH measure_vmps AS (
             OR (
                 vtm.vtm IN (
                 '774624002', -- VTM code for apixaban
-                '777455008', -- VTM code for rivaroxaban
+                '777455008' -- VTM code for rivaroxaban
                 )
                 AND
-                ofr.name = 'tablet.oral' -- only oral MR tablet formulations in numerator
+                ofr.name != 'tablet.oral' -- only NONE tablet formulations in numerator as these are higher cost
             )
              THEN 'numerator'
             ELSE 'denominator'
         END as vmp_type
     FROM viewer_vmp vmp
-    JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-    JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-    JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+    LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+    LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+    LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+    WHERE vmp != '34819111000001102' -- exclude Rivaroxaban 15mg tablets and Rivaroxaban 20mg tablets from the measure as no DDD
 )
 SELECT 
     vmp.id as vmp_id,
     mv.vmp_type
 FROM viewer_vmp vmp
-JOIN measure_vmps mv ON mv.id = vmp.id
-JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+LEFT JOIN measure_vmps mv ON mv.id = vmp.id
+LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
 WHERE vtm.vtm IN (
     '774624002', -- VTM code for apixaban
     '777455008', -- VTM code for rivaroxaban
     '775732007', -- VTM code for edoxaban
     '13568411000001104'  -- 13568411000001103 is the VTM code for dabigatran - this has been manually changed to account for data upload issue - needs to be fixed before launching
 )
+AND vmp != '34819111000001102' -- exclude Rivaroxaban 15mg tablets and Rivaroxaban 20mg tablets from the measure as no DDD
