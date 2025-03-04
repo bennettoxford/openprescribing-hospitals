ed94395158300283ea8e82388db8ae30ef9c529f
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-04 18:30:03 +0000

    Update Phesgo measure
    Retain specific VMP selection.
    Move to new YAML format.
    Simplify SQL.
    For comparison with VTM based measure.

--- viewer/measures/phesgo/vmps.sql
+++ viewer/measures/phesgo/vmps.sql
@@ -1,9 +1,9 @@
 SELECT DISTINCT
     vmp.id,
     CASE 
-        WHEN vtm.vtm = '1306706004' THEN 'numerator' -- VTM code for Pertuzumab + Trastuzumab
+        WHEN vmp.code IN ('39332111000001100', '39332211000001106') THEN 'numerator' -- VMP codes for Pertuzumab + Trastuzumab products
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
 INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-WHERE vtm.vtm IN ('1306706004', '777824009')  -- VTM code for Pertuzumab + Trastuzumab, VTM code for Trastuzumab
+WHERE vmp.code IN ('39332111000001100', '39332211000001106', '41772111000001108', '35903811000001108', '23204911000001105', '41329811000001106', '23985411000001104', '23985511000001100')  -- VMP codes for selected Trastuzumab products
