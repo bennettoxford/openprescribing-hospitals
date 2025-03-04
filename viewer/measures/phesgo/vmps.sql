9077593895e58047dd58bc67e43d831a8e586bef
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-04 18:28:49 +0000

    Update Phesgo measure
    Change to new YAML based format.
    Simplify SQL.
    Move to use VTM rather than specific VMPs.
    Need to assess how additional products affect measure and are handled for "dose" unit.

---
+++ viewer/measures/phesgo/vmps.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    vmp.id,
+    CASE 
+        WHEN vtm.vtm = '1306706004' THEN 'numerator' -- VTM code for Pertuzumab + Trastuzumab
+        ELSE 'denominator'
+    END as vmp_type
+FROM viewer_vmp vmp
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+WHERE vtm.vtm IN ('1306706004', '777824009')  -- VTM code for Pertuzumab + Trastuzumab, VTM code for Trastuzumab
