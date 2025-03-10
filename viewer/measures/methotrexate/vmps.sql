b2bdeae6a5a7fd8a7b658646262684f79cf78d7a
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-10 12:47:53 +0000

    Update vmps.sql
    Simplify SQL

--- viewer/measures/methotrexate/vmps.sql
+++ viewer/measures/methotrexate/vmps.sql
@@ -1,15 +1,8 @@
-WITH measure_vmps AS (
-    SELECT DISTINCT
-        vmp.id,
-        CASE 
-            WHEN vmp.code = '41791911000001107' THEN 'numerator'
-            ELSE 'denominator'
-        END as vmp_type
-    FROM viewer_vmp vmp
-)
 SELECT DISTINCT
-    vmp.id as vmp_id,
-    mv.vmp_type
+    vmp.code
+    CASE
+        WHEN vmp.code = '41791911000001107' THEN 'numerator'
+        ELSE 'denominator'
+    END as vmp_type
 FROM viewer_vmp vmp
-LEFT JOIN measure_vmps mv ON mv.id = vmp.id
 WHERE vmp.code IN ('41791911000001107', '41792011000001100')
