2702c253386b041a54c42c5b13c59e67d03ce893
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 12:48:00 +0000

    Update doxazosin mr measure
    Simplify SQL
    Move to INNER JOIN
    Removed vtmidprev references - column not available

--- viewer/measures/lvp_doxazosin_mr/vmps.sql
+++ viewer/measures/lvp_doxazosin_mr/vmps.sql
@@ -1,28 +1,18 @@
-WITH measure_vmps AS (
-    SELECT DISTINCT
-        vmp.id,
-        CASE 
-            WHEN (
-                '773245005' IN (vtm.vtm, vtm.vtmidprev) -- VTM for doxazosin
-                AND
-                ofr.name = 'tabletmodified-release.oral' -- only oral MR tablet formulations in numerator
-            ) THEN 'numerator'
-            ELSE 'denominator'
-        END as vmp_type
-    FROM viewer_vmp vmp
-    LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-    LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-    LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
-)
 SELECT DISTINCT
     vmp.id as vmp_id,
-    mv.vmp_type
+    CASE 
+        WHEN (
+            vtm.vtm = '773245005' -- VTM for doxazosin
+            AND
+            ofr.name = 'tabletmodified-release.oral' -- only oral MR tablet formulations in numerator
+        ) THEN 'numerator'
+        ELSE 'denominator'
+    END as vmp_type
 FROM viewer_vmp vmp
-LEFT JOIN measure_vmps mv ON mv.id = vmp.id
-LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+INNER JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+INNER JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
 WHERE 
-    '773245005' IN (vtm.vtm, vtm.vtmidprev) -- VTM for doxazosin
+    vtm.vtm = '773245005' -- VTM for doxazosin
     AND 
     ofr.name IN ('tabletmodified-release.oral', 'tablet.oral') -- oral tablet/MR tablet formulations in denominator
