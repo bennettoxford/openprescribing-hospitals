2f677f56e6b817efa62da5085af9a01357589503
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 09:11:48 +0000

    Update co-proxamol SQL

--- viewer/measures/lvp_coproxamol/vmps.sql
+++ viewer/measures/lvp_coproxamol/vmps.sql
@@ -1,17 +1,8 @@
-WITH measure_vmps AS (
-    SELECT DISTINCT
-        vmp.id,
-        CASE 
-            WHEN '18037311000001104' IN (vtm.vtm, vtm.vtmidprev) THEN 'numerator' -- VTM for co-proxamol
-            ELSE 'denominator'
-        END as vmp_type
-    FROM viewer_vmp vmp
-    LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-)
 SELECT DISTINCT
     vmp.id as vmp_id,
-    mv.vmp_type
+    CASE 
+        WHEN '18037311000001104' IN (vtm.vtm, vtm.vtmidprev) THEN 'numerator' -- VTM for co-proxamol
+    END as vmp_type -- this measure has no denominator
 FROM viewer_vmp vmp
-LEFT JOIN measure_vmps mv ON mv.id = vmp.id
 LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
 WHERE '18037311000001104' IN (vtm.vtm, vtm.vtmidprev) -- VTM for co-proxamol
