dd2efb85a08430d5d99eebc5939085a5190dcb00
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 12:48:17 +0000

    Update perindopril arginine measure
    Simplify SQL
    Move to INNER JOIN
    Removed vtmidprev references - column not available

--- viewer/measures/lvp_perindopril_arginine/vmps.sql
+++ viewer/measures/lvp_perindopril_arginine/vmps.sql
@@ -1,30 +1,20 @@
-WITH measure_vmps AS (
-    SELECT DISTINCT
-        vmp.id,
-        CASE 
-            WHEN (
-                '22209111000001100' IN (vtm.vtm, vtm.vtmidprev) -- VTM for perindopril arginine
-            ) THEN 'numerator'
-            ELSE 'denominator'
-        END as vmp_type
-    FROM viewer_vmp vmp
-    LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-    LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-    LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
-)
 SELECT DISTINCT
     vmp.id as vmp_id,
-    mv.vmp_type
+    CASE 
+        WHEN (
+            vtm.vtm = '22209111000001100' -- VTM for perindopril arginine
+        ) THEN 'numerator'
+        ELSE 'denominator'
+    END as vmp_type 
 FROM viewer_vmp vmp
-LEFT JOIN measure_vmps mv ON mv.id = vmp.id
-LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
-LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+    INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+    INNER JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+    INNER JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
 WHERE 
     (
-        '22209111000001100' IN (vtm.vtm, vtm.vtmidprev) -- VTM for perindopril arginine
+        vtm.vtm = '22209111000001100' -- VTM for perindopril arginine
         OR
-        '22209011000001101' IN (vtm.vtm, vtm.vtmidprev) -- VTM for perindopril erbumine
+        vtm.vtm = '22209011000001101' -- VTM for perindopril erbumine
     )
     AND 
     ofr.name = 'tablet.oral' -- oral tablet
