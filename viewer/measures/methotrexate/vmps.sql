0de8d1efd601a0fd50af26efb0f494aee9164c13
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 08:59:37 +0000

    Update vmps.sql

--- viewer/measures/methotrexate/vmps.sql
+++ viewer/measures/methotrexate/vmps.sql
@@ -1,8 +1,11 @@
 SELECT DISTINCT
-    vmp.code
+    vmp.id as vmp_id,
     CASE
-        WHEN vmp.code = '41791911000001107' THEN 'numerator'
+        WHEN '41791911000001107' IN (vtm.code, vtm.vtmidprev) THEN 'numerator' -- VMP code for methotrexate 10mg tablets
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
-WHERE vmp.code IN ('41791911000001107', '41792011000001100')
+WHERE
+    '41791911000001107' IN (vtm.code, vtm.vtmidprev) -- VMP code for methotrexate 10mg tablets
+    OR
+    '41792011000001100' IN (vtm.code, vtm.vtmidprev) -- VMP code for methotrexate 2.5mg tablets
