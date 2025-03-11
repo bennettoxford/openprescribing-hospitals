c31ece0104861387615ccd610a35db31220789a0
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 12:47:41 +0000

    Update co-proxamol measure
    Simplify SQL
    Move to INNER JOIN
    Removed vtmidprev references - column not available

--- viewer/measures/lvp_coproxamol/vmps.sql
+++ viewer/measures/lvp_coproxamol/vmps.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
     CASE 
-        WHEN '18037311000001104' IN (vtm.vtm, vtm.vtmidprev) THEN 'numerator' -- VTM for co-proxamol
+        WHEN vtm.vtm = '18037311000001104' THEN 'numerator' -- VTM for co-proxamol
     END as vmp_type -- this measure has no denominator
 FROM viewer_vmp vmp
 LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
-WHERE '18037311000001104' IN (vtm.vtm, vtm.vtmidprev) -- VTM for co-proxamol
+WHERE vtm.vtm = '18037311000001104' -- VTM for co-proxamol
