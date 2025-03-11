22c9ba639bf77cd6ea3275589cf5c8a7a306c49d
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-03-11 12:53:36 +0000

    Update methotrexate measure
    Fix VMP code reference.
    Remove reference to prev id - column not available

--- viewer/measures/methotrexate/vmps.sql
+++ viewer/measures/methotrexate/vmps.sql
@@ -1,11 +1,11 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
     CASE
-        WHEN '41791911000001107' IN (vtm.code, vtm.vtmidprev) THEN 'numerator' -- VMP code for methotrexate 10mg tablets
+        WHEN vmp.code = '41791911000001107' THEN 'numerator' -- VMP code for methotrexate 10mg tablets
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
 WHERE
-    '41791911000001107' IN (vtm.code, vtm.vtmidprev) -- VMP code for methotrexate 10mg tablets
+    vmp.code = '41791911000001107' -- VMP code for methotrexate 10mg tablets
     OR
-    '41792011000001100' IN (vtm.code, vtm.vtmidprev) -- VMP code for methotrexate 2.5mg tablets
+    vmp.code = '41792011000001100' -- VMP code for methotrexate 2.5mg tablets
