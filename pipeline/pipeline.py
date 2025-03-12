2ae22ad5dd840c34cb8837177635634a4a5c1979
Louis Fisher <louis.fisher1919@gmail.com>
2025-03-12 15:18:14 +0000

    add flow to import org ae status

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,6 +3,7 @@ from prefect import flow, get_run_logger
 from setup_bq_tables import setup_tables
 from import_unit_conversion import import_unit_conversion_flow
 from import_organisations import import_organisations
+from import_org_ae_status import import_ae_status
 @flow(name="SCMD Import Pipeline")
 def scmd_pipeline():
     logger = get_run_logger()
@@ -10,6 +11,7 @@ def scmd_pipeline():
     setup_tables()
     import_unit_conversion_flow()
     import_organisations()
+    import_ae_status()
     logger.info("SCMD Import Pipeline completed")
     
 if __name__ == "__main__":
