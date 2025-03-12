39389337d0b6251753d163a32575ac1e39d49a8f
Louis Fisher <louis.fisher1919@gmail.com>
2025-03-12 15:11:55 +0000

    add flow to load unit conversion table

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,11 +1,13 @@
 from prefect import flow, get_run_logger
 
 from setup_bq_tables import setup_tables
+from import_unit_conversion import import_unit_conversion_flow
 @flow(name="SCMD Import Pipeline")
 def scmd_pipeline():
     logger = get_run_logger()
     logger.info("Starting SCMD Import Pipeline")
     setup_tables()
+    import_unit_conversion_flow()
     logger.info("SCMD Import Pipeline completed")
     
 if __name__ == "__main__":
