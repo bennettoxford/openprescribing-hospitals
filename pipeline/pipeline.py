837f55e59455d6a9f84884284a9a240179e7a2e1
Louis Fisher <louis.fisher1919@gmail.com>
2025-03-12 15:14:28 +0000

    add flow to import organisations

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,12 +2,14 @@ from prefect import flow, get_run_logger
 
 from setup_bq_tables import setup_tables
 from import_unit_conversion import import_unit_conversion_flow
+from import_organisations import import_organisations
 @flow(name="SCMD Import Pipeline")
 def scmd_pipeline():
     logger = get_run_logger()
     logger.info("Starting SCMD Import Pipeline")
     setup_tables()
     import_unit_conversion_flow()
+    import_organisations()
     logger.info("SCMD Import Pipeline completed")
     
 if __name__ == "__main__":
