8ddddde2f82a0e1f5de7974b330f58efa2811ffe
Louis Fisher <louis.fisher1919@gmail.com>
2025-03-12 15:22:13 +0000

    add flow to import atc ddd now stored in GCS

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,6 +5,7 @@ from import_unit_conversion import import_unit_conversion_flow
 from import_organisations import import_organisations
 from import_org_ae_status import import_ae_status
 from import_adm_route_mapping import import_adm_route_mapping_flow
+from import_atc_ddd import import_ddd_atc_flow
 @flow(name="SCMD Import Pipeline")
 def scmd_pipeline():
     logger = get_run_logger()
@@ -14,6 +15,7 @@ def scmd_pipeline():
     import_organisations()
     import_ae_status()
     import_adm_route_mapping_flow()
+    import_ddd_atc_flow()
     logger.info("SCMD Import Pipeline completed")
     
 if __name__ == "__main__":
