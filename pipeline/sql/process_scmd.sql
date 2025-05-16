ef0ff131eab650beb0d26bf2eb8803e77927c98d
Louis Fisher <louis.fisher1919@gmail.com>
2025-05-16 10:12:25 +0100

    use history table for vmp normalisation instead of the previous codes available in the dm+d table, which only include the most recent previous vmp code - there can be more than 1 change

--- pipeline/sql/process_scmd.sql
+++ pipeline/sql/process_scmd.sql
@@ -2,10 +2,17 @@ CREATE OR REPLACE TABLE `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ SCMD_PROCESSED_TAB
 PARTITION BY year_month
 CLUSTER BY vmp_code
 AS
-WITH normalized_vmps AS (
+WITH history_mappings AS (
+  SELECT 
+    previous_id,
+    current_id
+  FROM `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ DMD_HISTORY_TABLE_ID }}`
+  WHERE entity_type = 'VMP'
+  AND previous_id != current_id
+),
+normalized_vmps AS (
   SELECT 
     CAST(vmp_code AS STRING) as vmp_code,
-    CAST(vmp_code_prev AS STRING) as vmp_code_prev,
     vmp_name
   FROM `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ DMD_TABLE_ID }}`
 ),
@@ -14,14 +21,16 @@ normalized_data AS (
     CAST(raw.year_month AS DATE) as year_month,
     CAST(raw.ods_code AS STRING) as ods_code,
     CAST(raw.vmp_snomed_code AS STRING) as vmp_snomed_code,
-    CAST(COALESCE(dmd.vmp_code, raw.vmp_snomed_code) AS STRING) as vmp_code,
+    CAST(COALESCE(map.current_id, raw.vmp_snomed_code) AS STRING) as vmp_code,
     CAST(COALESCE(dmd.vmp_name, raw.vmp_product_name) AS STRING) as vmp_name,
     CAST(raw.unit_of_measure_identifier AS STRING) as uom_id,
     CAST(raw.total_quantity_in_vmp_unit AS FLOAT64) as quantity,
     CAST(raw.indicative_cost AS FLOAT64) as indicative_cost
   FROM `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ SCMD_RAW_TABLE_ID }}` raw
+  LEFT JOIN history_mappings map
+    ON CAST(raw.vmp_snomed_code AS STRING) = map.previous_id
   LEFT JOIN normalized_vmps dmd
-    ON CAST(raw.vmp_snomed_code AS STRING) = dmd.vmp_code_prev
+    ON COALESCE(map.current_id, raw.vmp_snomed_code) = dmd.vmp_code
 ),
 unit_conversions AS (
   SELECT 
