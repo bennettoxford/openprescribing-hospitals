7b3216cfbe2bf27632f2f1c46091ca37241d6c26
Louis Fisher <louis.fisher1919@gmail.com>
2025-07-03 09:54:21 +0100

    drop login requirement for product lookup

--- viewer/views.py
+++ viewer/views.py
@@ -923,7 +923,7 @@ class AboutView(TemplateView):
         context = super().get_context_data(**kwargs)
         return context
 
-@method_decorator(login_required, name='dispatch')
+
 class ProductDetailsView(TemplateView):
     template_name = "product_details.html"
     
@@ -937,7 +937,6 @@ class ProductDetailsView(TemplateView):
         return context
 
 @csrf_protect
-@login_required
 @api_view(["POST"])
 def product_details_api(request):
     try:
