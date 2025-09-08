fa3e12f1c5cb57304284c1f5fc58131f10627721
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-09-08 12:41:45 +0100

    Setup measure

---
+++ viewer/measures/atezolizumab_sc/vmps.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    vmp.id as vmp_id,
+    CASE 
+        WHEN ofr.name = 'solutioninjection.subcutaneous' THEN 'numerator' -- only subcutaneous formulations in numerator
+        WHEN ofr.name = 'solutioninfusion.intravenous' THEN 'denominator'
+    END as vmp_type
+FROM viewer_vmp vmp
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+INNER JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+INNER JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+WHERE vtm.vtm = '774689009' -- Atezolizumab VTM
