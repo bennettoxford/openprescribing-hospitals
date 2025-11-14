54c1757ae3de3e631bc5499b0c3bb2e794e71108
LFISHER7 <louis.fisher1919@gmail.com>
2025-11-14 09:44:18 +0000

    use dose calculation logic when calculating doses

--- pipeline/quantity/calculate_doses.sql
+++ pipeline/quantity/calculate_doses.sql
@@ -18,48 +18,36 @@ WITH combined_data AS (
     vmp.df_ind,
     vmp.udfs,
     vmp.udfs_uom,
-    vmp.udfs_basis_quantity,
-    vmp.udfs_basis_uom,
-    vmp.unit_dose_uom,
-    vmp.unit_dose_basis_uom
+    calc_logic.can_calculate_dose,
+    calc_logic.dose_calculation_logic,
+    calc_logic.udfs_basis_quantity,
+    calc_logic.udfs_basis_uom,
+    calc_logic.unit_dose_uom,
+    calc_logic.unit_dose_basis_uom
   FROM `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ SCMD_PROCESSED_TABLE_ID }}` AS processed
   LEFT JOIN `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ ORGANISATION_TABLE_ID }}` AS org
     ON processed.ods_code = org.ods_code
   LEFT JOIN `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ VMP_TABLE_ID }}` AS vmp
     ON processed.vmp_code = vmp.vmp_code
+  LEFT JOIN `{{ PROJECT_ID }}.{{ DATASET_ID }}.{{ DOSE_CALCULATION_LOGIC_TABLE_ID }}` AS calc_logic
+    ON processed.vmp_code = calc_logic.vmp_code
 ),
 calculated_doses AS (
   SELECT 
     *,
     CASE 
-      WHEN df_ind = 'Discrete' THEN
-        CASE
-          WHEN normalised_uom_name = udfs_basis_uom THEN 
-            normalised_quantity / udfs_basis_quantity
-          WHEN normalised_uom_name = unit_dose_uom THEN 
-            normalised_quantity
-          ELSE NULL
-        END
+      WHEN dose_calculation_logic = 'SCMD quantity / UDFS'
+      THEN normalised_quantity / udfs_basis_quantity
+      WHEN dose_calculation_logic = 'Direct quantity'
+      THEN normalised_quantity
       ELSE NULL
     END AS dose_quantity,
     CASE 
-      WHEN df_ind = 'Discrete' AND 
-           (normalised_uom_name = udfs_basis_uom OR 
-            normalised_uom_name = unit_dose_uom)
+      WHEN can_calculate_dose
       THEN unit_dose_uom
       ELSE NULL
     END AS dose_unit,
-    CASE 
-      WHEN df_ind = 'Discrete' THEN
-        CASE
-          WHEN normalised_uom_name = udfs_basis_uom THEN 
-            'SCMD quantity / UDFS'
-          WHEN normalised_uom_name = unit_dose_uom THEN 
-            'Direct quantity'
-          ELSE 'Not calculated: Unit mismatch'
-        END
-      ELSE 'Not calculated: not a discrete form'
-    END AS calculation_logic
+    dose_calculation_logic AS calculation_logic
   FROM combined_data
 )
 SELECT 
@@ -73,14 +61,7 @@ SELECT
   normalised_uom_id AS scmd_basis_unit,
   normalised_uom_name AS scmd_basis_unit_name,
   normalised_quantity AS scmd_quantity_in_basis_units,
-  udfs,
-  udfs_uom,
-  udfs_basis_quantity,
-  udfs_basis_uom,
-  unit_dose_uom,
-  unit_dose_basis_uom,
   dose_quantity,
   dose_unit,
-  df_ind,
   calculation_logic
 FROM calculated_doses
