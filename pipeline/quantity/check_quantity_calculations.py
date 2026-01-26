b02f7634fcddb811bde600c40875a7c5a7f41dff
LFISHER7 <louis.fisher1919@gmail.com>
2026-01-26 17:16:46 +0000

    add to pipeline

--- pipeline/quantity/check_quantity_calculations.py
+++ pipeline/quantity/check_quantity_calculations.py
@@ -6,6 +6,8 @@ from prefect import flow, get_run_logger
 from pipeline.quantity.dose_calculation_logic import dose_calculation_logic
 from pipeline.quantity.ingredient_calculation_logic import ingredient_calculation_logic
 from pipeline.quantity.ddd_calculation_logic import ddd_calculation_logic
+from pipeline.atc_ddd.ddd_comments.populate_ddd_route_comments import populate_ddd_route_comments_table
+from pipeline.quantity.handle_ddd_route_comments import handle_ddd_route_comments
 from pipeline.atc_ddd.ddd_comments.populate_ddd_refers_to import populate_ddd_refers_to_table
 from pipeline.quantity.ddd_comment_handling_refers_to import ddd_comment_handling_refers_to
 from pipeline.atc_ddd.ddd_comments.populate_ddd_expressed_as import populate_ddd_expressed_as_table
@@ -20,6 +22,8 @@ def check_quantity_calculations():
     dose_calculation_logic()
     ingredient_calculation_logic()
     ddd_calculation_logic()
+    populate_ddd_route_comments_table()
+    handle_ddd_route_comments()
     populate_ddd_refers_to_table()
     ddd_comment_handling_refers_to()
     populate_ddd_expressed_as_table()
