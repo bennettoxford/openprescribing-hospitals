993d97f13d325913b3c2618e78a0211a7f93dcb1
speed-vm <77046757+speed-vm@users.noreply.github.com>
2026-02-24 16:58:35 +0000

    New atropine measure

---
+++ viewer/measures/atropine_pfs/vmps.sql
@@ -0,0 +1,18 @@
+WITH injectable_atropine AS (
+    SELECT DISTINCT
+        vmp.id AS vmp_id,
+        CASE 
+            WHEN vmp.unit_dose_uom = 'ampoule'
+                THEN 'numerator'
+            ELSE 'denominator'
+        END AS vmp_type
+    FROM viewer_vmp vmp
+    INNER JOIN viewer_vtm vtm 
+        ON vtm.id = vmp.vtm_id
+    INNER JOIN viewer_vmp_ont_form_routes vofr 
+        ON vofr.vmp_id = vmp.id
+    INNER JOIN viewer_ontformroute ofr 
+        ON ofr.id = vofr.ontformroute_id
+    WHERE vtm.vtm = '774696006'  -- Atropine VTM
+      AND ofr.name = 'solutioninjection.intravenous'
+)
