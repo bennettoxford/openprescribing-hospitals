bbc1be8c84ec94ec1ebff08b8cbf257f0ee52a05
speed-vm <77046757+speed-vm@users.noreply.github.com>
2026-02-25 17:05:29 +0000

    thrombopoietin (TPO) receptor agonists measure Generic eltrombopag measure

---
+++ viewer/measures/tpo_agonist/vmp.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT
+    vmp.id as vmp_id,
+    CASE 
+        WHEN vtm.vtm != '775745006' -- eltrombopag
+        THEN 'numerator'
+        ELSE 'denominator'
+    END as vmp_type
+FROM viewer_vmp vmp
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+WHERE 
+    vtm.vtm IN (
+        '775745006', -- eltrombopag
+        '777463009', -- rompiplostim
+        '774706009', -- avatrombopag
+    )
