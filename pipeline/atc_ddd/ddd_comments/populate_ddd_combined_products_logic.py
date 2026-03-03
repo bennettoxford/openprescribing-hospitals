60d5cfc2e0befa2e331227b888e645960f394de3
LFISHER7 <louis.fisher1919@gmail.com>
2026-03-03 13:09:08 +0000

    validate schema when populated combined products

--- pipeline/atc_ddd/ddd_comments/populate_ddd_combined_products_logic.py
+++ pipeline/atc_ddd/ddd_comments/populate_ddd_combined_products_logic.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 from prefect import flow, get_run_logger
 
-from pipeline.utils.utils import execute_bigquery_query_from_sql_file
+from pipeline.utils.utils import execute_bigquery_query_from_sql_file, validate_table_schema
+from pipeline.setup.bq_tables import DDD_COMBINED_PRODUCTS_LOGIC_TABLE_SPEC
 
 
 @flow(name="Populate DDD combined products logic")
@@ -19,6 +20,9 @@ def populate_ddd_combined_products_logic():
     execute_bigquery_query_from_sql_file(str(sql_file_path))
 
     logger.info("DDD combined products logic table populated")
+
+    validate_table_schema(DDD_COMBINED_PRODUCTS_LOGIC_TABLE_SPEC)
+    logger.info("DDD combined products logic table schema validation completed")
 
 
 if __name__ == "__main__":
