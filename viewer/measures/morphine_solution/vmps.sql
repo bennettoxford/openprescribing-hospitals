853747b9f80c1c4202e17a88f83a7583cf8bed38
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2026-03-17 09:15:10 +0000

    Update vmps.sql

--- viewer/measures/morphine_solution/vmps.sql
+++ viewer/measures/morphine_solution/vmps.sql
@@ -10,6 +10,8 @@ WITH measure_vmps AS (
         END as vmp_type
     FROM viewer_vmp vmp
     LEFT JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+    LEFT JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+    LEFT JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id  
 )
 SELECT DISTINCT
     vmp.id as vmp_id,
