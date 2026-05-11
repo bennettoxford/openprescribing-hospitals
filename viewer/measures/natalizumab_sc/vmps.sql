24cc10ef80b6ed52b43248ec1e43b528334aacd3
LFISHER7 <louis.fisher1919@gmail.com>
2026-05-11 14:34:42 +0100

    fix natalizumab sql

--- viewer/measures/natalizumab_sc/vmps.sql
+++ viewer/measures/natalizumab_sc/vmps.sql
@@ -1,11 +1,11 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
-    CASE
-        WHEN vmp.code = '36030311000001108' THEN 'numerator' -- VMP code for Natalizumab 300mg/15ml solution for infusion vials
-        ELSE 'denominator'
+    CASE 
+        WHEN ofr.name = 'solutioninjection.subcutaneous' THEN 'numerator'
+        WHEN ofr.name = 'solutioninfusion.intravenous' THEN 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
-WHERE
-    vmp.code = '39819411000001106' -- VMP code for Natalizumab 150mg/1ml solution for injection pre-filled syringes
-    OR
-    vmp.code = '36030311000001108' -- VMP code for Natalizumab 300mg/15ml solution for infusion vials
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
+INNER JOIN viewer_vmp_ont_form_routes vofr ON vofr.vmp_id = vmp.id
+INNER JOIN viewer_ontformroute ofr ON ofr.id = vofr.ontformroute_id
+WHERE vtm.vtm = '776876003' -- Natalizumab
