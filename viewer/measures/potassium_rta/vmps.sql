67e0e4dd85772547ad3007c17b602ee04a15779c
speed-vm <77046757+speed-vm@users.noreply.github.com>
2026-05-12 12:06:29 +0100

    Update vmps.sql
    amending strength for including products, needs to >40mmol/L which is equivalent to 0.3% so have opted for 0.2% to cover relevant products

--- viewer/measures/potassium_rta/vmps.sql
+++ viewer/measures/potassium_rta/vmps.sql
@@ -1,8 +1,7 @@
 -- Denominator:
--- KCl or potassium dihydrogen phosphate classified as HIGH STRENGTH:
--- 10% potassium chloride solutions or greater
--- (i.e. >= 0.1 g per 1 ml when expressed as gram/ml,
--- or >= 100 mg per 1 ml when expressed as mg/ml),
+-- KCl or potassium dihydrogen phosphate with strength > 0.2% w/v
+-- (i.e. > 0.002 g per 1 ml when expressed as gram/ml,
+-- or > 2 mg per 1 ml when expressed as mg/ml),
 -- unit dose UOM in the injectable list.
 -- Require a VTM (vtm_id IS NOT NULL) so VMPs with no VTM are excluded from the measure,
 -- including e.g. VMP 5239011000001108 (Generic Addiphos solution for infusion 20ml vials).
@@ -28,11 +27,11 @@ AND EXISTS (
       AND (
           (
               vis.strnt_nmrtr_uom_name = 'gram'
-              AND vis.strnt_nmrtr_val >= 0.1
+              AND vis.strnt_nmrtr_val >= 0.002
           )
           OR (
               vis.strnt_nmrtr_uom_name IN ('mg', 'milligram')
-              AND vis.strnt_nmrtr_val > 100
+              AND vis.strnt_nmrtr_val > 2
           )
       )
 )
