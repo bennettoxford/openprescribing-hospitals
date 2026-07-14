a30388bb5ae58c6a80f6a8f56d9f046aec8db171
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2026-07-14 09:49:29 +0100

    Adjust exclusion code

--- viewer/measures/lvp_rubefacients/vmps.sql
+++ viewer/measures/lvp_rubefacients/vmps.sql
@@ -3,24 +3,28 @@ SELECT DISTINCT
     'numerator' as vmp_type -- this measure has no denominator
 FROM viewer_vmp vmp
 WHERE 
-    vmp.bnf_code LIKE '10030200%' -- Rubefacients, topical NSAIDS, capsaicin and poultice
+    vmp.bnf_code IN ('1003020', '10030200') -- Rubefacients, topical NSAIDS, capsaicin and poultice
     AND 
-    vmp.bnf_code NOT LIKE '10030200AD%' -- Arnica montana
+    vmp.vtm NOT LIKE '9874511000001102' -- Arnica montana
     AND 
-    vmp.bnf_code NOT LIKE '10030200AA%' -- Capsaicin
+    vmp.vtm NOT LIKE '775036003' -- Capsaicin
     AND 
-    vmp.bnf_code NOT LIKE '10030200AF%' -- Diclofenac diethyl
+    vmp.vtm NOT LIKE '32889211000001103' -- Diclofenac diethyl
     AND 
-    vmp.bnf_code NOT LIKE '10030200U0%' -- Diclofenac sodium
+    vmp.vtm NOT LIKE '36409011000001100' -- Diclofenac sodium
     AND 
-    vmp.bnf_code NOT LIKE '10030200M0%' -- Felbinac
+    vmp.vtm NOT LIKE '775922005' -- Felbinac
     AND 
-    vmp.bnf_code NOT LIKE '10030200W0%' -- Heparinoid
+    vmp.vtm NOT LIKE '776180006' -- Heparinoid
     AND 
-    vmp.bnf_code NOT LIKE '10030200P0%' -- Ibuprofen
+    vmp.vtm NOT LIKE '776287003' -- Ibuprofen
     AND 
-    vmp.bnf_code NOT LIKE '10030200L0%' -- Kaolin Heavy
+    vmp.vtm NOT LIKE '776288008' -- Ibuprofen + Levomenthol
     AND 
-    vmp.bnf_code NOT LIKE '1003020010%' -- Ketoprofen
+    vmp.vtm NOT LIKE '776444003' -- Kaolin Heavy
     AND 
-    vmp.bnf_code NOT LIKE '10030200R0%' -- Piroxicam
+    vmp.vtm NOT LIKE '776450008' -- Ketoprofen
+    AND 
+    vmp.vtm NOT LIKE '777223005' -- Piroxicam
+    AND
+    vmp.vtm NOT LIKE '20448811000001100' -- Amitriptyline + Ketamine
