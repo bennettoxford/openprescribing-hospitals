3d28d06a53fa048b1216609738afa250bb6b200b
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2026-07-14 11:19:47 +0100

    Update vmps.sql

--- viewer/measures/lvp_rubefacients/vmps.sql
+++ viewer/measures/lvp_rubefacients/vmps.sql
@@ -2,10 +2,11 @@ SELECT DISTINCT
     vmp.id as vmp_id,
     'numerator' as vmp_type -- this measure has no denominator
 FROM viewer_vmp vmp
+INNER JOIN viewer_vtm vtm ON vtm.id = vmp.vtm_id
 WHERE 
     vmp.bnf_code IN ('1003020', '10030200') -- Rubefacients, topical NSAIDS, capsaicin and poultice
     AND 
-    vmp.vtm_id NOT IN 
+    vtm.vtm NOT IN 
         (
             '9874511000001102', -- Arnica montana
             '775036003', -- Capsaicin
