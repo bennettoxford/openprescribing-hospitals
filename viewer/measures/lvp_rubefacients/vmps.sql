3da8f5acf4a305a79fdce425b343d93dff5aa597
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2026-07-14 10:59:38 +0100

    Update vmps.sql

--- viewer/measures/lvp_rubefacients/vmps.sql
+++ viewer/measures/lvp_rubefacients/vmps.sql
@@ -5,26 +5,18 @@ FROM viewer_vmp vmp
 WHERE 
     vmp.bnf_code IN ('1003020', '10030200') -- Rubefacients, topical NSAIDS, capsaicin and poultice
     AND 
-    vmp.vtm_id NOT LIKE '9874511000001102' -- Arnica montana
-    AND 
-    vmp.vtm_id NOT LIKE '775036003' -- Capsaicin
-    AND 
-    vmp.vtm_id NOT LIKE '32889211000001103' -- Diclofenac diethyl
-    AND 
-    vmp.vtm_id NOT LIKE '36409011000001100' -- Diclofenac sodium
-    AND 
-    vmp.vtm_id NOT LIKE '775922005' -- Felbinac
-    AND 
-    vmp.vtm_id NOT LIKE '776180006' -- Heparinoid
-    AND 
-    vmp.vtm_id NOT LIKE '776287003' -- Ibuprofen
-    AND 
-    vmp.vtm_id NOT LIKE '776288008' -- Ibuprofen + Levomenthol
-    AND 
-    vmp.vtm_id NOT LIKE '776444003' -- Kaolin Heavy
-    AND 
-    vmp.vtm_id NOT LIKE '776450008' -- Ketoprofen
-    AND 
-    vmp.vtm_id NOT LIKE '777223005' -- Piroxicam
-    AND
-    vmp.vtm_id NOT LIKE '20448811000001100' -- Amitriptyline + Ketamine
+    vmp.vtm_id NOT IN 
+        (
+            '9874511000001102', -- Arnica montana
+            '775036003', -- Capsaicin
+            '32889211000001103', -- Diclofenac diethyl
+            '36409011000001100', -- Diclofenac sodium
+            '775922005', -- Felbinac
+            '776180006', -- Heparinoid
+            '776287003', -- Ibuprofen
+            '776288008', -- Ibuprofen + Levomenthol
+            '776444003', -- Kaolin Heavy
+            '776450008', -- Ketoprofen
+            '777223005', -- Piroxicam
+            '20448811000001100' -- Amitriptyline + Ketamine
+        )
