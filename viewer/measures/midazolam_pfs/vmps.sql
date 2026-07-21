2238a33080603d5b51732781b4229efbc9e2da77
speed-vm <77046757+speed-vm@users.noreply.github.com>
2026-07-21 18:40:25 +0100

    Update VMP codes for Midazolam in SQL query
    updated strength to user specified 50mg/50m;

--- viewer/measures/midazolam_pfs/vmps.sql
+++ viewer/measures/midazolam_pfs/vmps.sql
@@ -1,11 +1,11 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
     CASE
-        WHEN vmp.code = '7977911000001102' THEN 'numerator' -- VMP code for Midazolam 100mg/50ml solution for infusion vials
+        WHEN vmp.code = '4508111000001108' THEN 'numerator' -- VMP code for Midazolam 50mg/50ml solution for infusion vials
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
 WHERE
-    vmp.code = '7977911000001102' -- VMP code for Midazolam 100mg/50ml solution for infusion vials
+    vmp.code = '4508111000001108' -- VMP code for Midazolam 50mg/50ml solution for infusion vials
     OR
-    vmp.code = '38082511000001102' -- VMP code for Midazolam 100mg/50ml solution for infusion pre-filled syringes
+    vmp.code = '22710611000001108' -- VMP code for Midazolam 50mg/50ml solution for infusion pre-filled syringes
