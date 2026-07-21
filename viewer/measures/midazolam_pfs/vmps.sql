0e98973915fb987f97f8e3bdfa7b26b43f7b76e6
Caroline <90867424+carolineacuda@users.noreply.github.com>
2026-07-21 16:47:58 +0100

    Update vmps.sql
    correction of vmp cofe

--- viewer/measures/midazolam_pfs/vmps.sql
+++ viewer/measures/midazolam_pfs/vmps.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
     CASE
-        WHEN vmp.code = '36072211000001102' THEN 'numerator' -- VMP code for Ephedrine 30mg/1ml solution for injection ampoules
+        WHEN vmp.code = '7977911000001102' THEN 'numerator' -- VMP code for Midazolam 100mg/50ml solution for infusion vials
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
