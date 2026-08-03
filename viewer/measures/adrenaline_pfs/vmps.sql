fe25ecdeeabcfcfed7592b06eddd72c45e0b8c78
speed-vm <77046757+speed-vm@users.noreply.github.com>
2026-08-03 16:17:55 +0100

    Update VMP code description for Adrenaline

--- viewer/measures/adrenaline_pfs/vmps.sql
+++ viewer/measures/adrenaline_pfs/vmps.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT
     vmp.id as vmp_id,
     CASE
-        WHEN vmp.code = '35894911000001108' THEN 'numerator' -- VMP code for Adrenaline (base) 1mg/10ml (1 in 10,000) dilute solution for injection ampoules
+        WHEN vmp.code = '35894911000001108' THEN 'numerator' -- VMP code for Adrenaline (base) 100micrograms/1ml (1 in 10,000) dilute solution for injection ampoules
         ELSE 'denominator'
     END as vmp_type
 FROM viewer_vmp vmp
